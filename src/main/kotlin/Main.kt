7f614d5213baaa86401682212a57d90536b285c7
Dayssol Joseph <dayssol@newsaktuell.de>
2023-04-25 15:31:38 +0200

    Updating Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,6 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
-
     val myAge : Int = 37
     val isTeenager : Boolean
     if(myAge <= 19 && myAge >= 13 ){
